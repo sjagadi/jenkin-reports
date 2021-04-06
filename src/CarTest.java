0dfe8277307932e62e6eb288e13ccf3a1332cebd
Santoshkumar Jagadi <santoshkumar.jagadi@gmail.com>
2021-04-06 09:32:51 +0200

    added failing tests

--- src/CarTest.java
+++ src/CarTest.java
@@ -16,19 +16,19 @@ public class CarTest {
 
     @Test
     public void testMake2(){
-        System.out.println("Running Test 1!");
+        System.out.println("Running Test 2!");
         Assertions.assertEquals(make, testCar.getMake(),"Expected vehicle make did not match actual vehicle make"); 
     }
 
     @Test
     public void testMake3(){
-        System.out.println("Running Test 1!");
-        Assertions.assertEquals(make, testCar.getMake(),"Expected vehicle make did not match actual vehicle make"); 
+        System.out.println("Running Test 3!");
+        Assertions.assertEquals(make, "Hondaa","Expected vehicle make did not match actual vehicle make"); 
     }
 
     @Test 
     public void testModel(){
-        System.out.println("Running Test 2!");
+        System.out.println("Running Test 4!");
         Assertions.assertEquals(model,testCar.getModel(),"Expected vehicle model did not match actual vehicle model"); 
     }
     
